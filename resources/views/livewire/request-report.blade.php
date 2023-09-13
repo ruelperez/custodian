a182e7653b1dbe373686b2557c68dae40f0f2174
ruelperez <karuelflorperezz@gmail.com>
2023-09-13 13:20:18 +0800

    Update request-report.blade.php
    replaced save icon to print icon, adjust margin left

--- resources/views/livewire/request-report.blade.php
+++ resources/views/livewire/request-report.blade.php
@@ -4,8 +4,8 @@
     @endif
     <h5 style="margin-left: 1%;">Request Report</h5>
     <div style="display: flex;">
-        <div style="margin-left: 37%;margin-top: 4%;">
-            <i title="Save Form" class="fa-solid fa-file-arrow-down" style="font-size: 30px; cursor: pointer; color: #0a53be" onclick="window.location='{{ route('form-inventory.pdf',['request' => 'inventory'])}}'"></i>
+        <div style="margin-left: 82%;margin-top: 4%;">
+            <i title="Print Form" class="fa-solid fa-print" style="font-size: 25px; cursor: pointer; color: #0a53be" onclick="window.location='{{ route('form-inventory.pdf',['request' => 'inventory'])}}'"></i>
         </div>
     </div>
 
