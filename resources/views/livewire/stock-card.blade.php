6129159bd552c444b46c1b8e0e9ba64184253d2d
ruelperez <karuelflorperezz@gmail.com>
2023-09-16 14:01:09 +0800

    Update stock-card.blade.php
    move print icon into table div

--- resources/views/livewire/stock-card.blade.php
+++ resources/views/livewire/stock-card.blade.php
@@ -3,17 +3,17 @@
         <i class="fa-solid fa-backward" style="font-size: 20px; cursor: pointer; margin-top: 3%; margin-bottom: 2%;" onclick="clickBk4()" wire:click="clickBack"></i>
     @endif
     <h5 style="margin-left: 1%;">Stock Card</h5>
-        <div style="display: flex; margin-top: 3%;">
-            <div style="margin-left: 45%;">
-                <h4 style="text-align: center; margin-top: 3%;">{{ucwords($itemName)}}</h4>
-            </div>
-            <div style="margin-left: 30%; margin-top: 1%;">
-                <i title="Print" class="fa-solid fa-print" style="font-size: 25px; cursor: pointer; color: #0a53be" onclick="window.location='{{ route('form-inventory.pdf',['request' => 'inventory'])}}'"></i>
-            </div>
-        </div>
 
-        <div style="margin-left: 15%;width: 70%;margin-top: 1%; margin-bottom: 2%;">
-            <table class="table table-hover" style="width: 100%; text-align: center">
+        <div style="margin-left: 15%;width: 70%;margin-top: 2%; margin-bottom: 2%;">
+            <div style="display: flex;">
+                <div>
+                    <h4 style="text-align: center; margin-top: 3%;">{{ucwords($itemName)}}</h4>
+                </div>
+                <div style="margin-left: auto; margin-top: 1%;">
+                    <i title="Print" class="fa-solid fa-print" style="font-size: 25px; cursor: pointer; color: #0a53be" onclick="window.location='{{ route('form-inventory.pdf',['request' => 'inventory'])}}'"></i>
+                </div>
+            </div>
+            <table class="table table-hover" style="width: 100%; text-align: center; margin-top: 1%;">
                 <thead>
                 <tr class="inv">
                     <th>
