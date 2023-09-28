cf4ec45ebc955278e3a0197428189e2907c1b92a
ruelperez <karuelflorperezz@gmail.com>
2023-09-28 15:01:11 +0800

    enable sort in teacher table

--- app/Http/Livewire/TeacherItem.php
+++ app/Http/Livewire/TeacherItem.php
@@ -8,7 +8,7 @@ use Livewire\Component;
 
 class TeacherItem extends Component
 {
-    public $receiver_id, $qtyPass = 0, $qtyNotModel, $waste_id, $unit, $movedData, $qty = 0, $item_name, $teacher_name, $receiver_name, $deployed_data, $ff=0, $hover_id;
+    public $receiver_id, $qtyPass = 0, $sort1 = "item_name", $sort2 = "desc", $qtyNotModel, $waste_id, $unit, $movedData, $qty = 0, $item_name, $teacher_name, $receiver_name, $deployed_data, $ff=0, $hover_id;
 
 
     public function render()
@@ -24,6 +24,15 @@ class TeacherItem extends Component
     public function displayData(){
         $this->deployed_data = BackupPrepare::where('receiver','=', $this->teacher_name)
             ->where('item_type', '!=', 'consumable')
+            ->orderBy($this->sort1,$this->sort2)
             ->get();
+    }
+
+    public function clickSort1($name){
+        $this->sort1 = $name;
+    }
+
+    public function clickSort2($name){
+        $this->sort2 = $name;
     }
 }
--- resources/views/livewire/teacher-item.blade.php
+++ resources/views/livewire/teacher-item.blade.php
@@ -3,8 +3,20 @@
         <div style="margin-left: 10%;">
             <p style="font-size: 18px;">{{ucwords($teacher_name)}}</p>
         </div>
-        <div style="margin-left: 70%;">
-            <i class="fa-solid fa-sort" style="font-size: 19px; cursor: pointer"></i>
+        <div style="margin-left: 64%;">
+            <div class="dropdown">
+                <a class="btn btn-info dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false">
+                    <i class="fa-solid fa-sort" style="font-size: 19px; cursor: pointer;"></i>
+                    Sort
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+                    <li><a class="dropdown-item" wire:click="clickSort1('item_name')" style="cursor: pointer;">Name @if($sort1 == "item_name") <b style="font-size: 30px; margin-left: 3%;">&middot;</b> @endif</a></li>
+                    <li><a class="dropdown-item" wire:click="clickSort1('created_at')" style="cursor: pointer;">Date @if($sort1 == "created_at") <b style="font-size: 30px; margin-left: 3%;">&middot;</b> @endif</a></li>
+                    <hr>
+                    <li><a class="dropdown-item" wire:click="clickSort2('asc')" style="cursor: pointer;">Ascending @if($sort2 == "asc") <b style="font-size: 30px; margin-left: 3%;">&middot;</b> @endif</a></li>
+                    <li><a class="dropdown-item" wire:click="clickSort2('desc')" style="cursor: pointer;">Descending @if($sort2 == "desc") <b style="font-size: 30px; margin-left: 3%;">&middot;</b> @endif</a></li>
+                </ul>
+            </div>
         </div>
     </div>
     <div style="display: flex; width: 100%;">
