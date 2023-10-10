43840c0315eca9e17a001f6adec61cde0a00c3b2
Nesjohn Joelle M. Felices <nesjohnfelices@gmail.com>
2023-10-10 13:51:15 +0800

    Update header.blade.php
    add title

--- resources/views/partial/header.blade.php
+++ resources/views/partial/header.blade.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="{{ asset('css/bootstrap.css') }}">
+    <title>Tandaay Property Custodian Management System</title>
     <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/fontawesome.min.css')}}">
     <link rel="stylesheet" href="{{ asset('css/design.css')}}">
