25392d72a966e65b334adaff01035b9893f8832e
ruelperez <karuelflorperezz@gmail.com>
2023-10-12 01:31:28 +0800

    put select month for pie chart

--- resources/views/livewire/graph.blade.php
+++ resources/views/livewire/graph.blade.php
@@ -17,21 +17,30 @@
                     <canvas id="myChart" height="400" style="width: 390%;"></canvas>
                 </div>
             </div>
-            <div>
-                <select>
-                    <option selected>January</option>
-                    <option value="1">February</option>
-                    <option value="2">March</option>
-                    <option value="3">April</option>
-                    <option value="3">May</option>
-                    <option value="3">June</option>
-                    <option value="3">July</option>
-                    <option value="3">August</option>
-                    <option value="3">September</option>
-                    <option value="3">October</option>
-                    <option value="3">November</option>
-                    <option value="3">December</option>
-                </select>
+            <div style="display: flex;">
+                <div>
+                    <select>
+                        <option selected>January</option>
+                        <option value="1">February</option>
+                        <option value="2">March</option>
+                        <option value="3">April</option>
+                        <option value="3">May</option>
+                        <option value="3">June</option>
+                        <option value="3">July</option>
+                        <option value="3">August</option>
+                        <option value="3">September</option>
+                        <option value="3">October</option>
+                        <option value="3">November</option>
+                        <option value="3">December</option>
+                    </select>
+                </div>
+                <div>
+                    <div class="map_canvas" style="margin-top: 3%; margin-right: 1.5%;">
+
+                        <canvas id="myPie" height="300" style="width: 370%;"></canvas>
+                    </div>
+                </div>
+
             </div>
         </div>
         <div>
@@ -79,6 +88,55 @@
 <script src="{{ asset('js/chart.js') }}"></script>
 
 <script>
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: <?php echo json_encode($item_name) ?>,
+            datasets: [{
+                label: '',
+                data: <?php echo json_encode($item_quantity); ?>,
+                backgroundColor: [
+                    'rgba(31, 58, 147, 1)',
+                    'rgba(37, 116, 169, 1)',
+                    'rgba(92, 151, 191, 1)',
+                    'rgb(200, 247, 197)',
+                    'rgb(77, 175, 124)',
+                    'rgb(30, 130, 76)'
+                ],
+                borderColor: [
+                    'rgba(31, 58, 147, 1)',
+                    'rgba(37, 116, 169, 1)',
+                    'rgba(92, 151, 191, 1)',
+                    'rgb(200, 247, 197)',
+                    'rgb(77, 175, 124)',
+                    'rgb(30, 130, 76)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    max: 300,
+                    min: 0,
+                    ticks: {
+                        stepSize: 50
+                    }
+                }
+            },
+            plugins: {
+                title: {
+                    display: false,
+                    text: 'Custom Chart Title'
+                },
+                legend: {
+                    display: false,
+                }
+            }
+        }
+    });
+
     var ctx = document.getElementById('myChart').getContext('2d');
     var myChart = new Chart(ctx, {
         type: 'bar',
