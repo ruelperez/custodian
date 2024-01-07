1ecad13d5f0c9b0d92eccd476b4b4f72b6dc00c8
ruelperez <karuelflorperezz@gmail.com>
2024-01-08 07:52:44 +0800

    create livewire "designation"

---
+++ app/Http/Livewire/Designation.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Designation extends Component
+{
+    public function render()
+    {
+        return view('livewire.designation');
+    }
+}
