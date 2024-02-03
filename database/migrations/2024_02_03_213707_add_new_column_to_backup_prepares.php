19209eca72457ca60369607527d958e5e449513a
ruelperez <karuelflorperezz@gmail.com>
2024-02-03 21:40:02 +0800

    Update 2024_02_03_213707_add_new_column_to_backup_prepares.php
    put column named item_id

--- database/migrations/2024_02_03_213707_add_new_column_to_backup_prepares.php
+++ database/migrations/2024_02_03_213707_add_new_column_to_backup_prepares.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('backup_prepares', function (Blueprint $table) {
-            $table->string('transaction_name');
+            $table->string('item_id')->nullable();
         });
     }
 
