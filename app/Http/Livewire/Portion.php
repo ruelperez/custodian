621d530901d1ee8cd5ffb17171df60f41e09e8ac
ruelperez <karuelflorperezz@gmail.com>
2024-02-19 12:27:23 +0800

    save

--- app/Http/Livewire/Portion.php
+++ app/Http/Livewire/Portion.php
@@ -9,11 +9,8 @@ use Livewire\Component;
 
 class Portion extends Component
 {
-<<<<<<< HEAD
-    public $option = "graph",$mons, $registrationSwitch, $username, $current, $new, $retype, $item_type, $mos, $report, $df=0, $hover;
-=======
+
     public $option = "graph",$mons, $username, $current, $new, $retype, $item_type, $mos, $report, $df=0, $hover;
->>>>>>> add-parameter
 
     public function render()
     {
